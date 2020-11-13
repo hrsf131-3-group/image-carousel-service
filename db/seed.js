be54457e540f91d9e129d67a5a4ec2b5bbde80bd
Shailee Patel <shaileeee@gmail.com>
2020-11-13 01:55:18 -0800

    promisify db.connection

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,25 @@
 var db = require('./index.js');
 var faker = require('faker');
+var Promise = require('bluebird');
+
+Promise.promisifyAll(db.connection);
 
 var seedListings = (n) => {
-  
-  var values = [];
-  for (var i = 0; i < n; i++) {
-    values.push([faker.name.findName()]);
-  }
-  var query = 'INSERT INTO listings (name) VALUES ?';
-  db.connection.query(query, [values], (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('created');
-    }
-  });
+  db.connection.queryAsync('DELETE FROM listings')
+    .then(() => {
+      var values = [];
+      for (var i = 0; i < n; i++) {
+        values.push([faker.name.findName()]);
+      }
+      var query = 'INSERT INTO listings (name) VALUES ?';
+      return db.connection.queryAsync(query, [values]);
+    })
+    .then(() => {
+      console.log('successfully created');
+    })
+    .catch(() => {
+      console.log('error');
+    });
 };
 
 seedListings(10);
