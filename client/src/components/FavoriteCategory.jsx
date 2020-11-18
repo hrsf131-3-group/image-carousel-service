a7dcec09b98933dd55a0ce733fedd2774c5cba9c
Shailee Patel <shaileeee@gmail.com>
2020-11-18 12:51:58 -0800

    add modal for fav category

--- client/src/components/FavoriteCategory.jsx
+++ client/src/components/FavoriteCategory.jsx
@@ -4,21 +4,35 @@ import styled from 'styled-components';
 var FavoriteCategory = (props) => {
   return (
     <FavoriteCategoryContainer>
-      <h3>Save to a list</h3>
-      <div><button>Create a new list</button></div>
-      <div><button onClick = {props.onClickDone}>Done</button></div>
+      <ModalContainer>
+        <h3>Save to a list</h3>
+        <div><button>Create a new list</button></div>
+        <div><button onClick = {props.onClickDone}>Done</button></div>
+      </ModalContainer>
     </FavoriteCategoryContainer>
   );
 };
 
 var FavoriteCategoryContainer = styled.div`
   position: absolute;
-  left: 0px;
-  top: 0px;
-  z-index: 1;
-  background-color: white;
+  top: 0;
+  left: 0;
+  z-index: 2;
   width: 100%;
   height: 100%;
+  background-color: rgba(0,0,0,0.7);
+`;
+
+var ModalContainer = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 35%;
+  z-index: 3;
+  background-color: white;
+  width: 570px;
+  height: 360px;
+  text-align: center;
+  border-radius: 15px;
 `;
 
 export default FavoriteCategory;
