6c1f6c3300ff54c04c50cc5cd54d34a4d518f7fb
Shailee Patel <shaileeee@gmail.com>
2020-11-20 00:14:41 -0800

    make the show all photos button

--- client/src/components/Images.jsx
+++ client/src/components/Images.jsx
@@ -9,11 +9,14 @@ var Images = (props) => {
       <Img3 src = {props.images[2].url} onClick = {() => props.onClickImage(2)}/>
       <Img4 src = {props.images[3].url} onClick = {() => props.onClickImage(3)}/>
       <Img5 src = {props.images[4].url} onClick = {() => props.onClickImage(4)}/>
+      <Button>
+        <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17" role="presentation" aria-hidden="true" focusable="false"><circle cx="1.5" cy="1.5" r="1.5"/><circle cx="1.5" cy="8.5" r="1.5"/><circle cx="8.5" cy="1.5" r="1.5"/><circle cx="8.5" cy="8.5" r="1.5"/><circle cx="15.5" cy="1.5" r="1.5"/><circle cx="15.5" cy="8.5" r="1.5"/><circle cx="1.5" cy="15.5" r="1.5"/><circle cx="8.5" cy="15.5" r="1.5"/><circle cx="15.5" cy="15.5" r="1.5"/></Svg>Show all photos</Button>
     </ImageGrid>
   );
 };
 
 var ImageGrid = styled.div`
+  position: relative;
   display: grid;
   grid-template-columns: 2fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
@@ -54,6 +57,36 @@ var Img4 = styled(Img1)`
 
 var Img5 = styled(Img1)`
   grid-area: 2 / 3 / 3 / 4;
+`;
+
+var Button = styled.button`
+  & {
+    position: absolute;
+    right: 25px;
+    bottom: 25px;
+    background-color: white;
+    color: rgb(34, 34, 34);
+    font-size: 14px;
+    padding: 7px 15px;
+    border: 1px solid rgb(34, 34, 34);
+    cursor: pointer;
+    border-radius: 8px;
+    font-weight: 500;
+    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
+  }
+
+  &:hover {
+    border-color: rgb(0, 0, 0);
+    background: rgb(247, 247, 247);
+    color: rgb(34, 34, 34);
+  }
+`;
+
+var Svg = styled.svg`
+  height: 12px;
+  width: 12px;
+  fill: currentcolor;
+  padding-right: 9px;
 `;
 
 export default Images;
